4b18ac1a2de2375baca5047b9acac22216e49281
hhjeee <yoonhj0315@gmail.com>
2024-01-06 20:09:12 +0900

    refactor: modify auth controller

--- src/service/member.service.ts
+++ src/service/member.service.ts
@@ -3,20 +3,27 @@ import MemberRepository from '../repository/member.repository';
 import { InternalServerError } from '../util/customErrors';
 
 export default class MemberService {
-  /*static async registerMember(member: MemberRegisterReqDTO): Promise<null> {
-    try {
-      await MemberRepository.create(member);
-      return null;
-    } catch (error) {
-      throw new InternalServerError('유저 정보를 저장하는데 실패했습니다.');
-    }
-  }*/
-
   static async getMemberByEmail(email: string): Promise<Member | null> {
     try {
       return await MemberRepository.findOne({ where: { email: email } });
     } catch (error) {
       throw new InternalServerError('유저 정보를 불러오는데 실패했습니다.');
+    }
+  }
+
+  static async saveMember(
+    email: string,
+    membername: string,
+    password: string,
+  ): Promise<Member> {
+    try {
+      return await MemberRepository.save({
+        email: email,
+        membername: membername,
+        password: password,
+      });
+    } catch (error) {
+      throw new InternalServerError('유저 정보를 저장하는데 실패했습니다.');
     }
   }
 }
