2f0409fb443c9c5e3f15984c48df2436951cc97a
unknown <dyj0304@korea.ac.kr>
2024-01-13 00:53:01 +0900

    feat: showTeam

--- src/controller/team/router.ts
+++ src/controller/team/router.ts
@@ -4,7 +4,7 @@ import {
   myTeam,
   searchTeam,
   updateTeam,
-  //showTeam,
+  showTeam,
   deleteTeam,
 } from './controller';
 import { isLoggedIn } from '../auth/controller';
@@ -15,7 +15,7 @@ teamRouter.post('/create', isLoggedIn, createTeam); // body를 사용하여 team
 teamRouter.get('/', isLoggedIn, myTeam); // userId에 맞는 team 정보 가져오기
 teamRouter.get('/search', isLoggedIn, searchTeam); // query를 사용하여 team 정보 가져오기
 teamRouter.patch('/:teamId', isLoggedIn, updateTeam); // param와 body를 사용하여 해당하는 team 정보 수정
-//teamRouter.get('/team/:teamId', isLoggedIn, showTeam); // param을 사용하여 해당하는 team 정보 가져오기
+teamRouter.get('/:teamId', isLoggedIn, showTeam); // param을 사용하여 해당하는 team 정보 가져오기
 teamRouter.delete('/:teamId', isLoggedIn, deleteTeam); // param을 사용하여 해당하는 team 정보 삭제
 
 export default teamRouter;
