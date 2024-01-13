05c81f19fbb5f6203dc055f3d2f9f8e62eed1623
wonjuneee <tony000709@gmail.com>
2024-01-13 23:19:44 +0900

    modify: team, memberTeam, alarm router & controller

--- src/controller/router.ts
+++ src/controller/router.ts
@@ -4,8 +4,8 @@ import teamRouter from './team/router';
 import noticeRouter from './notice/router';
 import authRouter from './auth/router';
 import scheRouter from './schedule/router';
-import teamMemberRouter from './memberTeam/router';
 import memberRouter from './member/router';
+import alarmRouter from './alarm/router';
 
 const router = Router();
 
@@ -18,7 +18,7 @@ router.use('/notice', noticeRouter);
 router.use('/auth', authRouter);
 router.use('/schedule', scheRouter);
 router.use('/member', memberRouter);
-router.use('/team', teamMemberRouter);
+router.use('/alarm', alarmRouter);
 
 export default router;
 //main router
--- src/controller/team/router.ts
+++ src/controller/team/router.ts
@@ -7,9 +7,27 @@ import {
   //showTeam,
   deleteTeam,
 } from './controller';
+import {
+  toggleFavoriteTeam,
+  toggleHideTeam,
+  inviteMember,
+  toggleAdmin,
+  subsTeam,
+  unsubsTeam,
+} from '../memberTeam/controller';
+
 import { isLoggedIn } from '../auth/controller';
 
 const teamRouter = Router();
+
+teamRouter.patch('/favorite/:teamId', isLoggedIn, toggleFavoriteTeam);
+teamRouter.patch('/hide/:teamId', isLoggedIn, toggleHideTeam);
+
+teamRouter.post('/member', isLoggedIn, inviteMember);
+teamRouter.patch('/admin', isLoggedIn, toggleAdmin);
+
+teamRouter.post('/subscribe/:teamId', isLoggedIn, subsTeam);
+teamRouter.delete('/subscribe/:teamId', isLoggedIn, unsubsTeam);
 
 teamRouter.post('/create', isLoggedIn, createTeam); // body를 사용하여 team 정보 저장
 teamRouter.get('/', isLoggedIn, myTeam); // userId에 맞는 team 정보 가져오기
