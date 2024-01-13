8aae42d7b944ac3bd76feeb5099189042f692107
takmj00 <takmj00@gmail.com>
2024-01-13 18:36:37 +0900

    fix: isAdmin controller

--- src/repository/memberTeam.respository.ts
+++
@@ -1,6 +0,0 @@
-import AppDataSource from '../config/dataSource';
-import MemberTeam from '../entity/memberTeam.entity';
-
-const MemberTeamRepository = AppDataSource.getRepository(MemberTeam).extend({});
-
-export default MemberTeamRepository;
